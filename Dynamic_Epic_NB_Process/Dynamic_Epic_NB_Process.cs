afebf7cd1396c472fa2f13baf3c36d44bf6c18da
Jerrod Rathsack <JerrodR@americollect.com>
2020-03-17 09:06:38 -0500

    Add project files.

---
+++ Dynamic_Epic_NB_Process/Dynamic_Epic_NB_Process.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data
+
+namespace Dynamic_Epic_NB_Process
+{
+    class Dynamic_Epic_NB_Process
+    {
+        // store the credentials in the program for now, until worked on actual SSIS detail
+        const string connectionString_SQL = "DRIVER={MySQL ODBC 5.1 Driver};SERVER=amc-mysql.americollect.com;DATABASE=DataValidation;UID=DataValidation;PWD=FoVOyo2iheQi; OPTION=3;Connect Timeout=20;";
+
+        DataTable[] 
+    }
+}
